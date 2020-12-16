407cde398db300fa46085a7133084d30058d4ae3
Francisco Rivera Valverde <44504424+franchuterivera@users.noreply.github.com>
2020-12-16 09:15:11 +0100

    Just GitHub actions (#9)
    * Enabled github actions
    
    * Added pre-commit
    
    * Added git ignore

--- setup.py
+++ setup.py
@@ -1,15 +1,48 @@
+import sys
 from setuptools import setup
+
+
+# Raise warnings if system version is not greater than 3.5
+if sys.version_info < (3, 6):
+    raise ValueError(
+        'Unsupported Python version %d.%d.%d found. Pynisher requires Python '
+        '3.6 or higher.' % (sys.version_info.major, sys.version_info.minor, sys.version_info.micro)
+    )
+
+
+with open('README.rst') as fh:
+    long_description = fh.read()
 
 setup(
     name='pynisher',
     version="0.6.3",
     packages=['pynisher'],
     install_requires=['docutils>=0.3', 'setuptools', 'psutil'],
-    author="Stefan Falkner, Christina Hernandez-Wunsch, Samuel Mueller and Matthias Feurer",
+    extras_require={
+        "test": [
+            "pytest",
+            "pre-commit",
+            "pytest-cov",
+            "pytest-forked",
+            "flake8-import-order",
+            "scikit-learn",
+        ],
+    },
+    author="Stefan Falkner, Christina Hernandez-Wunsch, Samuel Mueller and Matthias Feurer and Francisco Rivera",
     author_email="feurerm@informatik.uni-freiburg.de",
     description="A small Python library to limit the resources used by a function by executing it inside a subprocess.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     include_package_data=False,
     keywords="resources",
     license="MIT",
     url="https://github.com/automl/pynisher",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.6',
 )
