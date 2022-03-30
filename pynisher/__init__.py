1f25db8a85648a7d9ad2f5da7215cad7d4537aed
René Sass <mail@renesass.de>
2022-03-30 12:08:10 +0200

    Nothing

--- pynisher/__init__.py
+++ pynisher/__init__.py
@@ -1,3 +1,4 @@
+import os
 from abc import abstractmethod
 from typing import Callable
 from pynisher.limit_function_call import *  # noqa
@@ -11,6 +12,7 @@ class Pynisher:
         cpu_time: int = None,
         wall_time: int = None,
         grace_period: int = None,
+        context=None,
     ) -> None:
         self.func = func
         self.memory = memory
@@ -18,14 +20,33 @@ class Pynisher:
         self.wall_time = wall_time
         self.grace_period = grace_period
 
+        if context is None:
+            self.context = os.environ.get("CONTEXT", "fork")
+        else:
+            self.context = context
+
     def __enter__(self) -> Callable:
+        """
+
+        Returns
+        -------
+        Callable
+            Returns limited function.
+        """
+
         limiter = Limiter(self.memory, self.cpu_time, self.wall_time, self.grace_period)
-        subprocess = self.context.Process(
-            target=limiter.__call__,
-            name="pynisher function call",
-            args=(self.func,) + args,
-            kwargs=kwargs,
-        )
+
+        def limited_func(*args, **kwargs):
+
+            parent_conn, child_conn = self.context.Pipe(duplex=False)
+            subprocess = self.context.Process(
+                target=limiter.__call__,
+                name="pynisher function call",
+                args=(self.func,) + args,
+                kwargs=kwargs,
+            )
+
+            subprocess.start()
 
         return self.limit
 
