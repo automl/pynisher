8a6d5d5e2b60b5e1df4b59339e11b4392f6ae1ba
eddiebergman <eddiebergmanhs@gmail.com>
2022-04-12 13:17:28 +0200

    Add documentation notes on signal handling

--- pynisher/limiters/mac.py
+++ pynisher/limiters/mac.py
@@ -1,16 +1,23 @@
-from typing import Any
+"""
+For documentation on MAC specific signal alarms:
+
+** `setrlimit` **
+https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/setrlimit.2.html
+
+** `signals` **
+https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/sigaction.2.html#//apple_ref/doc/man/2/sigaction
+"""
+from __future__ import annotations
+
 from pynisher.limiters.limiter import Limiter
 import signal
 import resource
 
 from pynisher.exceptions import (
-    MemorylimitException,
     CpuTimeoutException,
     TimeoutException,
     SignalException,
 )
-
-Frame = Any
 
 
 class LimiterMac(Limiter):
@@ -28,40 +35,97 @@ class LimiterMac(Limiter):
 
 
 class LimiterDarwin(Limiter):
+
     @staticmethod
-    def handler(signum: signal.Signals, *args: Any, **kwargs: Any) -> None:
+    def _handler(signum: signal.Signals, frame: signal.FrameType | None) -> None:
+        # SIGXCPU: cpu_time `setrlimit(RLIMIT_CPU, (soft, hard))`
+        #
+        #   Sent when process reaches `soft` limit of, then once a second until `hard`
+        #   before finally sending SIGKILL.
+        #   The default handler would just kill the process
         if signum == signal.SIGXCPU:
-            # When process reaches soft limit a SIGXCPU signal is sent, normally terminating the process
             raise CpuTimeoutException
+
+        # SIGALRM `signal.alarm(wall_time)`
+        #
+        #   When the alarm uses `wall_time`, SIGALARM will be sent.
+        #   It has no default action
         elif signum == signal.SIGALRM:
             # SIGALRM is sent to process when the specified time limit to an alarm function elapses
             # (when real or clock time elapses)
             raise TimeoutException
+
+        # UNKNOWN
+        #
+        #   We have caught some unknown signal. This means we are too restrictive
+        #   with the signals we are catching.
         else:
             raise SignalException
 
     def limit_memory(self, memory: int) -> None:
-        """Limit's the memory of this process."""
+        """Limit the addressable memory
+
+        This could technically raise `SIGSEGV` (segmentation fault) but
+        we instead catch a python `MemoryError` as indication that memory
+        time was exceeded. This lets us give back the traceback.
+
+        Parameters
+        ----------
+        memory : int
+            The memory limit in MB
+        """
         # Convert megabyte to byte
         mem_in_b = int(memory * 1024 * 1024)
         resource.setrlimit(resource.RLIMIT_AS, (mem_in_b, mem_in_b))
 
     def limit_cpu_time(self, cpu_time: int, grace_period: int = 0) -> None:
-        """Limit's the cpu time of this process."""
-        # From the Linux man page:
-        # When the process reaches the soft limit, it is sent a SIGXCPU signal.
-        # The default action for this signal is to terminate the process.
-        # However, the signal can be caught, and the handler can return control
-        # to the main program. If the process continues to consume CPU time,
-        # it will be sent SIGXCPU once per second until the hard limit is reached,
-        # at which time it is sent SIGKILL.
-        resource.setrlimit(
-            resource.RLIMIT_CPU,
-            (cpu_time, cpu_time + grace_period),
-        )
+        """Limit the cpu time for this process.
+
+        A SIGXCPU will be sent to the `_handler` once the `soft` limit
+        is reached, once per second until `hard` is reached and then
+        finally a SIGKILL.
+
+        Parameters
+        ----------
+        cpu_time : int
+            The amount of time in seconds
+
+        grace_period : int = 0
+            The amount of extra time given to the process before a SIGKILL
+            is sent.
+        """
+        soft = cpu_time
+        hard = cpu_time + grace_period
+
+        resource.setrlimit(resource.RLIMIT_CPU, (soft, hard))
+        signal.signal(signal.SIGALRM, LimiterDarwin._handler)
 
     def limit_wall_time(self, wall_time: int) -> None:
-        """Limit's the wall time of this process."""
+        """Limit the wall time for this process
 
-        signal.signal(signal.SIGALRM, LimiterDarwin.handler)
+        A SIGALARM will be sent to the handler once `wall_time` amount
+        of seconds has elapsed since the invocation of `signal.alarm(wall_time)`.
+
+        Parameters
+        ----------
+        wall_time : int
+            The amount of time to limit to in seconds
+        """
+        signal.signal(signal.SIGALRM, LimiterDarwin._handler)
         signal.alarm(wall_time)
+
+    def _debug_memory(self) -> str:
+        """Prints the memory used by the process
+
+        Returns
+        -------
+        (usage: str)
+            Returns the usage as a string, not sure if always KB but
+            leaving type as str until known.
+        """
+        # https://stackoverflow.com/a/39765583/5332072
+        with open('/proc/self/status') as f:
+            status = f.readlines()
+
+        vmpeak = next(s for s in status if s.startswith("VmPeak:"))
+        return vmpeak
