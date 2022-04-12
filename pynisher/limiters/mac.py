c72640df1f5328e1b2453b265bc11422af731abe
eddiebergman <eddiebergmanhs@gmail.com>
2022-04-12 13:40:56 +0200

    Try setting memory limit to RLIMIT_RSS for RAM

--- pynisher/limiters/mac.py
+++ pynisher/limiters/mac.py
@@ -1,11 +1,11 @@
 """
-For documentation on MAC specific signal alarms:
-
+For documentation on `setrlimit` and how to limit resources for MAC
 ** `setrlimit` **
 https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/setrlimit.2.html
 
+For documentation on MAC specific signal alarms:
 ** `signals` **
-https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/sigaction.2.html#//apple_ref/doc/man/2/sigaction
+https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/sigaction.2.html
 """
 from __future__ import annotations
 
@@ -76,7 +76,7 @@ class LimiterDarwin(Limiter):
         """
         # Convert megabyte to byte
         mem_in_b = int(memory * 1024 * 1024)
-        resource.setrlimit(resource.RLIMIT_AS, (mem_in_b, mem_in_b))
+        resource.setrlimit(resource.RLIMIT_RSS, (mem_in_b, mem_in_b))
 
     def limit_cpu_time(self, cpu_time: int, grace_period: int = 0) -> None:
         """Limit the cpu time for this process.
