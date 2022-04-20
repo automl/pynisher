69cae60d3763e5816a07a582825cfecfbb9ac8ec
eddiebergman <eddiebergmanhs@gmail.com>
2022-04-20 16:02:59 +0200

    Add warnings instead of failure for Windows

--- pynisher/limiters/windows.py
+++ pynisher/limiters/windows.py
@@ -1,15 +1,17 @@
 from pynisher.limiters.limiter import Limiter
 
+import warnings
+
 
 class LimiterWindows(Limiter):
     def limit_memory(self, memory: int) -> None:
         """Limit's the memory of this process."""
-        raise NotImplementedError()
+        warnings.warn("Currently `limit_memory` not implemented on Windows")
 
     def limit_cpu_time(self, cpu_time: int, grace_period: int = 1) -> None:
         """Limit's the cpu time of this process."""
-        raise NotImplementedError()
+        warnings.warn("Currently `limit_cpu_time` not implemented on Windows")
 
     def limit_wall_time(self, wall_time: int) -> None:
         """Limit's the wall time of this process."""
-        raise NotImplementedError()
+        warnings.warn("Currently `wall_time` not implemented on Windows")
