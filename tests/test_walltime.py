f8407e90c02e55fc5981f8a63601137938559b29
eddiebergman <eddiebergmanhs@gmail.com>
2022-04-20 16:06:18 +0200

    Add parametrization to test_walltime

--- tests/test_walltime.py
+++ tests/test_walltime.py
@@ -4,25 +4,23 @@ import time
 
 import pytest
 
+
 def func(sleep: float) -> bool:
     """Sleep for `sleep` seconds"""
     time.sleep(sleep)
     return True
 
 
-def test_fail() -> None:
+@pytest.mark.parametrize("wall_time", [1, 2])
+def test_fail(wall_time: int) -> None:
     """
     Expects
     -------
     * Should fail when the method uses more time than given
     """
     with pytest.raises(TimeoutException):
-        with Pynisher(func, wall_time=1) as restricted_func:
-            completed = False
-            completed = restricted_func(sleep=2)
-
-    assert not completed
-
+        with Pynisher(func, wall_time=wall_time) as restricted_func:
+            restricted_func(sleep=wall_time * 2)
 
 def test_success() -> None:
     """
