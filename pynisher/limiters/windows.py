e546370e11032dba5c36d1f01db66af279f9c698
eddiebergman <eddiebergmanhs@gmail.com>
2022-04-22 18:32:18 +0200

    Add windows memory limiter

--- pynisher/limiters/windows.py
+++ pynisher/limiters/windows.py
@@ -4,6 +4,7 @@ from typing import Any
 
 import signal
 import sys
+import traceback
 from threading import Timer
 
 from pynisher.exceptions import WallTimeoutException
@@ -44,7 +45,69 @@ class LimiterWindows(Limiter):
 
     def limit_memory(self, memory: int) -> None:
         """Limit's the memory of this process."""
-        self._raise_warning("Currently `limit_memory` not implemented on Windows")
+        # https://stackoverflow.com/a/54958772/5332072
+        try:
+
+            # First try to import stuff, an easy exception to catch and warn about
+            try:
+                import win32api
+                import win32job
+                import winerror
+            except ModuleNotFoundError as e:
+                self._raise_warning(
+                    f"{traceback.format_exc()}\n"
+                    f"Couldn't limit `memory` as `pywin32` is needed, try install with"
+                    f" `pip install pywin32`."
+                    f"\n* https://github.com/mhammond/pywin32"
+                    f"\n{e}"
+                )
+                return
+
+            # Here, we assign it to a windows "Job", whatever that is
+            # If the process is already assigned to a job, then we have
+            # to check if it's less than Windows 8 because apparently
+            # nested jobs aren't supported there
+            try:
+                job = win32job.CreateJobObject(None, "")
+                process = win32api.GetCurrentProcess()
+
+                win32job.AssignProcessToJobObject(job, process)
+
+            except win32job.error as e:
+                if (
+                    e.winerror != winerror.ERROR_ACCESS_DENIED
+                    or sys.getwindowsversion() >= (6, 2)  # type: ignore
+                    or not win32job.IsProcessInJob(process, None)
+                ):
+                    raise e
+                else:
+                    msg = (
+                        "The process is already in a job."
+                        " Nested jobs are not supported prior to Windows 8."
+                    )
+                    raise RuntimeError(msg) from e
+
+            # Now we can try limit memory
+            enum_for_info = win32job.JobObjectExtendedLimitInformation
+            info = win32job.QueryInformationJobObject(job, enum_for_info)
+
+            # This appears to by bytes by looking for "JOB_OBJECT_LIMIT_JOB_MEMORY"
+            # on github, specifically Python code
+            info["ProcessMemoryLimit"] = memory
+
+            # I think the is a mask to indicate the the memory should be limited
+            mask_limit_memory = win32job.JOB_OBJECT_LIMIT_PROCESS_MEMORY
+            info["BasicLimitInformation"]["LimitFlags"] |= mask_limit_memory
+
+            # Finally set the new information
+            win32job.SetInformationJobObject(job, enum_for_info, info)
+
+        except Exception as e:
+            self._raise_warning(
+                f"{traceback.format_exc()}\n"
+                f"Couldn't limit `memory` on Windows due to Error: {e}"
+            )
+            return
 
     def limit_cpu_time(self, cpu_time: int, grace_period: int = 1) -> None:
         """Limit's the cpu time of this process."""
@@ -63,7 +126,10 @@ class LimiterWindows(Limiter):
             self.timer = timer
 
         except Exception as e:
-            self._raise_warning(f"Couldn't limit `wall_time` on Windows due to {e}")
+            self._raise_warning(
+                f"Couldn't limit `wall_time` on Windows due to Error: {e}"
+                f"\n{traceback.format_exc()} "
+            )
 
     def _try_remove_memory_limit(self) -> bool:
         """Remove memory limit if it can"""
