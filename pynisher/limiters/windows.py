ca241d7d5290ebaf292479c59ee8287b87b264be
eddiebergman <eddiebergmanhs@gmail.com>
2022-04-26 02:40:33 +0200

    Fix flag for windows cpu limiting

--- pynisher/limiters/windows.py
+++ pynisher/limiters/windows.py
@@ -154,7 +154,7 @@ class LimiterWindows(Limiter):
         info["PerProcessUserTimeLimit"] = time
 
         # Activate the flag to turn on the limiting of cput time
-        flag = win32job.JOB_OBJECT_LIMIT_JOB_TIME
+        flag = win32job.JOB_OBJECT_LIMIT_PROCESS_TIME
         info["LimitFlags"] |= flag
 
         # Finally set the new information
