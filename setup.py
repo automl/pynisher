0408c0aac34a727d5109df2b5a54e15080797e23
eddiebergman <eddiebergmanhs@gmail.com>
2022-05-10 08:05:34 +0200

    Make 1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with readme.open("r") as fh:
 
 setup(
     name="pynisher",
-    version="0.7.0",
+    version="1.0.0",
     packages=find_packages(where=".", include=["pynisher*"], exclude=["test*"]),
     include_package_data=True,
     install_requires=[
