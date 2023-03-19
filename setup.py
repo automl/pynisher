dd22d4d69bc2e74b0a066a929cf341d2c20b0dc0
eddiebergman <eddiebergmanhs@gmail.com>
2023-03-19 13:59:21 +0100

    bump: 1.0.3 -> 1.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with readme.open("r") as fh:
 
 setup(
     name="pynisher",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(where=".", include=["pynisher*"], exclude=["test*"]),
     include_package_data=True,
     install_requires=[
